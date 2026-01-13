0cd60254fa83bef26ea06b8568b5f87a0ceac37d
Mohamed Mohamud <cloudchronicles92@gmail.com>
2026-01-13 00:35:57 +0000

    adding tmux nvim navigator

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,4 +1,23 @@
 return {
     "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-    "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+    {
+        "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+            { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+            { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+            { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+            { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+        },
+        init = function()
+            vim.g.tmux_navigator_no_mappings = 1
+        end,
+    },
 }
